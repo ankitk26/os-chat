5719393930499085a1e6e4af20710cc531733780
ankitk26 <myselfankit51@gmail.com>
2025-08-08 22:08:55 +0530

    modularize sources

--- src/components/ai-response-sources.tsx
+++ src/components/ai-response-sources.tsx
@@ -1,13 +1,15 @@
 import { UIMessage } from "ai";
-import { ChevronDownIcon, ChevronRightIcon, LinkIcon } from "lucide-react";
 import { useState } from "react";
-import ExternalLink from "./external-link";
-import { Badge } from "./ui/badge";
-import { Button } from "./ui/button";
+import SourcesList from "./sources-list";
+import SourcesToggleButton from "./sources-toggle-button";
 
 export default function AIResponseSources({ message }: { message: UIMessage }) {
   const [showSources, setShowSources] = useState(false);
   const sourceParts = message.parts.filter((part) => part.type === "source");
+
+  const toggleSourcesDisplay = () => {
+    setShowSources((prev) => !prev);
+  };
 
   if (sourceParts.length === 0) {
     return null;
@@ -16,40 +18,14 @@ export default function AIResponseSources({ message }: { message: UIMessage }) {
   return (
     <div className="mt-8">
       <div className="space-x-2 flex items-center">
-        <Button
-          size="icon"
-          variant="ghost"
-          onClick={() => {
-            setShowSources((prev) => !prev);
-          }}
-          className="cursor-pointer size-5 rounded"
-        >
-          {showSources ? (
-            <ChevronDownIcon className="size-4" />
-          ) : (
-            <ChevronRightIcon className="size-4" />
-          )}
-        </Button>
-
+        <SourcesToggleButton
+          toggleSourcesDisplay={toggleSourcesDisplay}
+          showSources={showSources}
+        />
         <div className="text-sm font-mono text-muted-foreground">Sources</div>
       </div>
 
-      {showSources && (
-        <div className="flex flex-wrap font-mono text-sm space-x-2 mt-2">
-          {sourceParts.map((sourcePart) => (
-            <Badge
-              key={sourcePart.source.id}
-              variant="secondary"
-              className="my-2"
-            >
-              <LinkIcon />
-              <ExternalLink href={sourcePart.source.url} className="underline">
-                {sourcePart.source.title}
-              </ExternalLink>
-            </Badge>
-          ))}
-        </div>
-      )}
+      {showSources && <SourcesList sourceParts={sourceParts} />}
     </div>
   );
 }
