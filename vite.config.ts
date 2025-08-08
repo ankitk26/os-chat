2ceca03a71bef22826dbdf23bd1ac3ccd185a5ad
ankitk26 <myselfankit51@gmail.com>
2025-08-08 21:47:47 +0530

    fix vite plugins and attempt to fix fouc

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
     tsconfigPaths(),
+    tailwindcss(),
     tanstackStart({
       target: "vercel",
+      customViteReactPlugin: true,
     }),
+    viteReact(),
   ],
 });
