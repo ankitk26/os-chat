25a56bfc4f203bba1a14d2fb7c7e2fd0c89fd4ce
ankitk26 <myselfankit51@gmail.com>
2025-08-22 01:51:57 +0530

    fix bug in markdown

--- src/components/ai-response-content.tsx
+++ src/components/ai-response-content.tsx
@@ -10,7 +10,7 @@ export default function AIResponseContent({
   messageId,
 }: Props) {
   return (
-    <div className="prose prose-neutral dark:prose-invert prose-rose prose-sm lg:prose-base prose-pre:m-0 w-full max-w-full overflow-x-auto break-words prose-pre:bg-transparent prose-pre:p-0 leading-7 lg:leading-8">
+    <div className="prose prose-neutral dark:prose-invert prose-rose prose-sm lg:prose-base prose-pre:m-0 w-full max-w-full break-words prose-pre:bg-transparent prose-pre:p-0 leading-7 lg:leading-8">
       <MemoizedMarkdown content={messageContent} id={messageId} />
     </div>
   );
