945caf3bee22f54be08d11526c8a7ce97dd45921
ankitk26 <myselfankit51@gmail.com>
2025-08-26 04:53:53 +0530

    remove suspense

--- src/routes/_auth.chat.$chatId.tsx
+++ src/routes/_auth.chat.$chatId.tsx
@@ -1,5 +1,5 @@
 import { convexQuery } from "@convex-dev/react-query";
-import { useSuspenseQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { createFileRoute, useRouteContext } from "@tanstack/react-router";
 import { api } from "convex/_generated/api";
 import Chat from "~/components/chat";
@@ -13,7 +13,7 @@ function RouteComponent() {
   const { chatId } = Route.useParams();
   const { auth } = useRouteContext({ from: "/_auth" });
 
-  const { data: messages, isPending: isMessagesPending } = useSuspenseQuery(
+  const { data: messages, isPending: isMessagesPending } = useQuery(
     convexQuery(api.messages.getMessages, {
       chatId,
       sessionToken: auth.session.token,
