58fc26e0f38bf275b7117d6e64236b2f381427fc
ankitk26 <myselfankit51@gmail.com>
2025-08-31 20:53:39 +0530

    clear on item click

--- src/components/app-sidebar-chat-item.tsx
+++ src/components/app-sidebar-chat-item.tsx
@@ -1,4 +1,5 @@
 import { Link } from "@tanstack/react-router";
+import { useSharedChatContext } from "~/providers/chat-provider";
 import type { SidebarChatType } from "~/types";
 import AppSidebarChatItemActions from "./app-sidebar-chat-item-actions";
 import BranchedChatIndicator from "./branched-chat-indicator";
@@ -8,10 +9,13 @@ type Props = {
 };
 
 export default function AppSidebarChatItem({ chat }: Props) {
+  const { clearChat } = useSharedChatContext();
+
   return (
     <Link
       activeProps={{ className: "bg-secondary text-secondary-foreground" }}
       className="group/chats flex cursor-pointer items-center justify-between rounded-md px-2 py-2 text-sm hover:bg-secondary hover:text-secondary-foreground dark:hover:bg-secondary dark:hover:text-secondary-foreground"
+      onClick={() => clearChat()}
       params={{ chatId: chat.uuid }}
       to="/chat/$chatId"
     >
