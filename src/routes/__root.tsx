d467e6fdb015a4c0cdcccd3ae0e4f9c6769b2385
ankitk26 <myselfankit51@gmail.com>
2026-02-10 04:10:12 +0530

    use fontsource

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -73,19 +73,6 @@ export const Route = createRootRouteWithContext<{
 				rel: "stylesheet",
 				href: appCss,
 			},
-			{
-				rel: "preconnect",
-				href: "https://fonts.googleapis.com",
-			},
-			{
-				rel: "preconnect",
-				href: "https://fonts.gstatic.com",
-				crossOrigin: "anonymous",
-			},
-			{
-				rel: "stylesheet",
-				href: "https://fonts.googleapis.com/css2?family=Geist+Mono:wght@100..900&family=Geist:wght@100..900&display=swap",
-			},
 		],
 	}),
 	component: RootComponent,
