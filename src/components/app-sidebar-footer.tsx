89a1e1c773e8ef1cac04dc59c3600a1e4fed5091
ankitk26 <myselfankit51@gmail.com>
2026-02-20 05:01:16 +0530

    fix link to settings and make destructive actions visible

--- src/components/app-sidebar-footer.tsx
+++ src/components/app-sidebar-footer.tsx
@@ -9,32 +9,34 @@ export default function AppSidebarFooter() {
 	return (
 		<SidebarMenu>
 			<SidebarMenuItem>
-				<SidebarMenuButton
-					render={<Link to="/settings" />}
-					size="lg"
-					tooltip={authUser?.name || "Settings"}
-				>
-					<Avatar className="h-8 w-8 rounded-lg grayscale">
-						<AvatarImage
-							alt={authUser?.name?.[0]}
-							src={authUser?.image ?? ""}
-						/>
-						<AvatarFallback className="rounded-lg">
-							{(authUser?.name ?? "")
-								.split(" ")
-								.slice(0, 2)
-								.map((namePart) => namePart.charAt(0))
-								.join("")}
-						</AvatarFallback>
-					</Avatar>
-					<div className="grid flex-1 text-left text-sm leading-tight">
-						<span className="truncate font-medium">{authUser?.name}</span>
-						<span className="text-muted-foreground truncate text-xs">
-							{authUser?.email}
-						</span>
-					</div>
-					<SettingsIcon className="ml-auto" />
-				</SidebarMenuButton>
+				<Link to="/settings" className="flex w-full">
+					<SidebarMenuButton
+						size="lg"
+						tooltip={authUser?.name || "Settings"}
+						className="w-full"
+					>
+						<Avatar className="h-8 w-8 rounded-lg grayscale">
+							<AvatarImage
+								alt={authUser?.name?.[0]}
+								src={authUser?.image ?? ""}
+							/>
+							<AvatarFallback className="rounded-lg">
+								{(authUser?.name ?? "")
+									.split(" ")
+									.slice(0, 2)
+									.map((namePart) => namePart.charAt(0))
+									.join("")}
+							</AvatarFallback>
+						</Avatar>
+						<div className="grid flex-1 text-left text-sm leading-tight">
+							<span className="truncate font-medium">{authUser?.name}</span>
+							<span className="text-muted-foreground truncate text-xs">
+								{authUser?.email}
+							</span>
+						</div>
+						<SettingsIcon className="ml-auto" />
+					</SidebarMenuButton>
+				</Link>
 			</SidebarMenuItem>
 		</SidebarMenu>
 	);
