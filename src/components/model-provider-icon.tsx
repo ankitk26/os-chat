84926c62fa4865a44072e965b66da6ec25542d23
ankitk26 <myselfankit51@gmail.com>
2026-02-25 01:05:42 +0530

    add zai and minimax models

--- src/components/model-provider-icon.tsx
+++ src/components/model-provider-icon.tsx
@@ -2,9 +2,11 @@ import type { JSX } from "react";
 import AnthropicIcon from "./anthropic-icon";
 import DeepSeekIcon from "./deepseek-icon";
 import GeminiIcon from "./gemini-icon";
+import MiniMaxIcon from "./minimax-icon";
 import MoonShotIcon from "./moonshot-icon";
 import OpenAIIcon from "./open-ai-icon";
 import XAIIcon from "./xai-icon";
+import ZaiIcon from "./zai-icon";
 
 type Props = {
 	provider: string;
@@ -17,6 +19,8 @@ const iconMap: { [key: string]: () => JSX.Element } = {
 	xai: XAIIcon,
 	deepseek: DeepSeekIcon,
 	moonshot: MoonShotIcon,
+	zai: ZaiIcon,
+	minimax: MiniMaxIcon,
 };
 
 export default function ModelProviderIcon({ provider }: Props) {
