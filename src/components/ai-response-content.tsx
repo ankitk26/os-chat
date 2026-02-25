e49111d41d1269184140bfe0a0721ce1640c52d2
ankitk26 <myselfankit51@gmail.com>
2026-02-26 00:20:35 +0530

    fix codeblock container issue

--- src/components/ai-response-content.tsx
+++ src/components/ai-response-content.tsx
@@ -11,7 +11,7 @@ export default function AIResponseContent({
 }: Props) {
 	return (
 		<div className="relative px-0 lg:-mx-2 lg:px-2">
-			<div className="prose prose-neutral dark:prose-invert prose-rose prose-sm w-full max-w-full leading-7 wrap-break-word select-text lg:leading-8 [&_pre]:overflow-x-auto [&_table]:block [&_table]:overflow-x-auto">
+			<div className="prose prose-neutral prose-pre:bg-transparent prose-pre:p-0 dark:prose-invert prose-rose prose-sm w-full max-w-full leading-7 wrap-break-word select-text lg:leading-8 [&_pre]:overflow-x-auto [&_table]:block [&_table]:overflow-x-auto">
 				<MemoizedMarkdown content={messageContent} id={messageId} />
 			</div>
 		</div>
