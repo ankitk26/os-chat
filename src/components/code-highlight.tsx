ef1caae92dd182425846e1f8db797303c84a981a
ankitk26 <myselfankit51@gmail.com>
2026-03-19 04:03:17 +0530

    feat: wrap code blocks

--- src/components/code-highlight.tsx
+++ src/components/code-highlight.tsx
@@ -1,8 +1,9 @@
-import { CopyIcon } from "@phosphor-icons/react";
+import { CopyIcon, TextAlignLeftIcon } from "@phosphor-icons/react";
 import { useTheme } from "next-themes";
-import type { ReactNode } from "react";
+import { useState, type ReactNode } from "react";
 import ShikiHighlighter, { type Element, isInlineCode } from "react-shiki";
 import { toast } from "sonner";
+import { cn } from "~/lib/utils";
 import { Button } from "./ui/button";
 import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip";
 
@@ -18,6 +19,7 @@ export default function CodeHighlight({
 	node,
 }: CodeHighlightProps) {
 	const { theme } = useTheme();
+	const [isWrap, setIsWrap] = useState(false);
 
 	const match = className?.match(/language-(\w+)/);
 	const language = match ? match[1] : undefined;
@@ -43,6 +45,23 @@ export default function CodeHighlight({
 							render={
 								<Button
 									className="dark:hover:bg-background/20"
+									onClick={() => setIsWrap(!isWrap)}
+									size="icon"
+									variant="ghost"
+								/>
+							}
+						>
+							<TextAlignLeftIcon />
+						</TooltipTrigger>
+						<TooltipContent>
+							{isWrap ? "Disable wrap" : "Enable wrap"}
+						</TooltipContent>
+					</Tooltip>
+					<Tooltip>
+						<TooltipTrigger
+							render={
+								<Button
+									className="dark:hover:bg-background/20"
 									onClick={async () => {
 										await navigator.clipboard.writeText(codeContent);
 										toast.success("Copied to clipboard");
@@ -59,9 +78,18 @@ export default function CodeHighlight({
 				</div>
 			</div>
 
-			<div className="bg-background dark:bg-code-dark w-full max-w-full overflow-x-auto rounded-br-lg rounded-bl-lg border-r border-b border-l text-xs sm:text-sm dark:border-0">
+			<div
+				className={cn(
+					"bg-background dark:bg-code-dark w-full max-w-full rounded-br-lg rounded-bl-lg border-r border-b border-l text-xs sm:text-sm dark:border-0",
+					isWrap ? "break-all whitespace-pre-wrap" : "overflow-x-auto",
+				)}
+			>
 				<ShikiHighlighter
-					className="text-sm"
+					className={cn(
+						"text-sm",
+						isWrap &&
+							"[&_code]:break-all [&_code]:!whitespace-pre-wrap [&_pre]:break-all [&_pre]:!whitespace-pre-wrap",
+					)}
 					delay={150}
 					language={language}
 					showLanguage={false}
